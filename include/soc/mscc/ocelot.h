82ffe44d07af656a210690df041f30f65edf270d
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-10-12 14:40:41 +0300

    net: dsa: tag_ocelot_8021q: break circular dependency with ocelot switch lib
    Michael reported that when using the "ocelot-8021q" tagging protocol,
    the switch driver module must be manually loaded before the tagging
    protocol can be loaded/is available.
    
    This appears to be the same problem described here:
    https://lore.kernel.org/netdev/20210908220834.d7gmtnwrorhharna@skbuf/
    where due to the fact that DSA tagging protocols make use of symbols
    exported by the switch drivers, circular dependencies appear and this
    breaks module autoloading.
    
    The ocelot_8021q driver needs the ocelot_can_inject() and
    ocelot_port_inject_frame() functions from the switch library. Previously
    the wrong approach was taken to solve that dependency: shims were
    provided for the case where the ocelot switch library was compiled out,
    but that turns out to be insufficient, because the dependency when the
    switch lib _is_ compiled is problematic too.
    
    We cannot declare ocelot_can_inject() and ocelot_port_inject_frame() as
    static inline functions, because these access I/O functions like
    __ocelot_write_ix() which is called by ocelot_write_rix(). Making those
    static inline basically means exposing the whole guts of the ocelot
    switch library, not ideal...
    
    We already have one tagging protocol driver which calls into the switch
    driver during xmit but not using any exported symbol: sja1105_defer_xmit.
    We can do the same thing here: create a kthread worker and one work item
    per skb, and let the switch driver itself do the register accesses to
    send the skb, and then consume it.
    
    Fixes: 0a6f17c6ae21 ("net: dsa: tag_ocelot_8021q: add support for PTP timestamping")
    Reported-by: Michael Walle <michael@walle.cc>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 49f885b2d97093451410e7279aa29d81e094e108)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    (conflicts in net/dsa/Kconfig with commit
     8b6e638b4be2 ("net: dsa: build tag_8021q.c as part of DSA core"))
    [Xulin: Original patch taken from
    https://github.com/vladimiroltean/linux/commits/felix-dsa-tag-vlan-stable]
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -733,39 +733,12 @@ struct ocelot_policer {
 #define ocelot_target_write(ocelot, target, val, reg) \
 	__ocelot_target_write_ix(ocelot, target, val, reg, 0)
 
-#if IS_ENABLED(CONFIG_MSCC_OCELOT_SWITCH_LIB)
-
 /* Packet I/O */
 bool ocelot_can_inject(struct ocelot *ocelot, int grp);
 void ocelot_port_inject_frame(struct ocelot *ocelot, int port, int grp,
 			      u32 rew_op, struct sk_buff *skb);
 int ocelot_xtr_poll_frame(struct ocelot *ocelot, int grp, struct sk_buff **skb);
 void ocelot_drain_cpu_queue(struct ocelot *ocelot, int grp);
-
-#else
-
-static inline bool ocelot_can_inject(struct ocelot *ocelot, int grp)
-{
-	return false;
-}
-
-static inline void ocelot_port_inject_frame(struct ocelot *ocelot, int port,
-					    int grp, u32 rew_op,
-					    struct sk_buff *skb)
-{
-}
-
-static inline int ocelot_xtr_poll_frame(struct ocelot *ocelot, int grp,
-					struct sk_buff **skb)
-{
-	return -EIO;
-}
-
-static inline void ocelot_drain_cpu_queue(struct ocelot *ocelot, int grp)
-{
-}
-
-#endif
 
 /* I/O */
 u32 ocelot_port_readl(struct ocelot_port *port, u32 reg);
